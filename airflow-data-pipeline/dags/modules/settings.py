fcdc6fbf4f4a1bfd89ee8f24cb9f8037f9020d71
davide <andreazzini.davide@gmail.com>
2022-08-15 15:13:23 -0300

    simple

--- airflow-data-pipeline/dags/modules/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-INBOUND_DIR = os.getenv("INBOUND_DIR", "inbound")
-DB_USER = os.getenv("DB_USER", "user")
-DB_PASS = os.getenv("DB_PASS", "pass")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_NAME = os.getenv("DB_NAME", "postgres")
-DB_PORT = os.getenv("DB_PORT", "5435")
