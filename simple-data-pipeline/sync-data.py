b8ff3baf239c297b64a83a8a08d57b22970ec0b1
davide <andreazzini.davide@gmail.com>
2022-08-18 23:30:32 -0300

    start

--- simple-data-pipeline/sync-data.py
+++ simple-data-pipeline/sync-data.py
@@ -7,8 +7,8 @@ from sqlalchemy import create_engine
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-inbound_dir = "/home/d4v3/dev/pygeekle22-airflow/simple-data-pipeline/inbound"
-process_dir = "/home/d4v3/dev/pygeekle22-airflow/simple-data-pipeline/processed"
+inbound_dir = ""
+process_dir = ""
 
 # create a script to read files from the inbound directory 
 # load the content of the file in a db table 
